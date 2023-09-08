b0b8e30f3747885e478e070f5c67c823cbc008fc
rwm275 <rwm275@cornell.edu>
2023-09-07 23:34:15 -0400

    bug fixes

--- l3/lvn.py
+++ l3/lvn.py
@@ -3,113 +3,192 @@ import json
 
 
 class Value:
-   def __init__(self, instr, var2num):
-      self.op = instr["op"]
-      self.args = []
-      self.isValue = "dest" in instr
-      if instr["op"] == "const":
-        self.val = instr["value"]
-      else:
-        if instr["args"]:
-          self.args = [var2num[x] for x in instr["args"]]
+    @classmethod
+    def constructor1(cls, instr, var2num):
+        value = Value()
+        value.op = instr["op"]
+        value.args = []
+        value.isValue = "dest" in instr
+        if instr["op"] == "const":
+            value.val = instr["value"]
+            value.type = instr["type"]
+        elif instr["op"] == "id":
+            value.val = instr["args"][0]
+            value.type = instr["type"]
         else:
-          self.isValue = False
-      self.args.sort()
+            if "args" in instr:
+                value.args = [var2num[x] for x in instr["args"]]
+            else:
+                value.isValue = False
+        value.args.sort()
+        return value
 
-   def __eq__(self, other):
-      if self.op != other.op:
-         return False
-      return self.args == other.args
+    @classmethod
+    def constructor2(cls, name):
+        value = Value()
+        value.op = "id"
+        value.val = name
+        value.type = "dummy"
+        return value
+    
+    @classmethod
+    def constructor3(cls, name, type):
+        value = Value()
+        value.op = "id"
+        value.val = name
+        value.type = type
+        return value
 
-   def __hash__(self):
-      if self.args:
-        return hash((self.op, *self.args))
-      return hash((self.op, self.val))
+    def __init__(self):
+        self.op = None
+        self.args = []
+        self.isValue = True
 
-   def __repr__(self):
-      res = "(" + self.op
-      if self.args:
-        for a in self.args:
-          res += ", " + str(a)
-      else:
-         res += " " + str(self.val)
-      return res + ")"
+    def __eq__(self, other):
+        if self.op != other.op:
+            return False
+        return self.args == other.args
+
+    def __hash__(self):
+        if self.args:
+            return hash((self.op, *self.args))
+        return hash((self.op, self.val, self.type))
+
+    def __repr__(self):
+        res = "(" + self.op
+        if self.args:
+            for a in self.args:
+                res += ", " + str(a)
+        else:
+            res += " " + str(self.val)
+        return res + ")"
 
 
 class Table():
-   def __init__(self):
-      self.valToRow = {}
-      self.rowToCanon = {}
+    def __init__(self):
+        self.valToRow = {}
+        self.rowToCanon = {}
 
-   def insert(self, row, value, canon):
-      self.valToRow[value] = row
-      self.rowToCanon[row] = canon
+    def insert(self, row, value, canon):
+        self.valToRow[value] = row
+        self.rowToCanon[row] = canon
 
-   def get(self, value):
-      return (self.valToRow[value], self.rowToCanon[self.valToRow[value]])
+    def get(self, value):
+        return (self.valToRow[value], self.rowToCanon[self.valToRow[value]])
 
-   def getFromRow(self, value):
-      return self.rowToCanon[value]
+    def getFromRow(self, value):
+        return self.rowToCanon[value]
 
-   def inTable(self, value):
-      return value in self.valToRow
+    def inTable(self, value):
+        return value in self.valToRow
+    
+    def remove(self, value):
+        row = self.valToRow[value]
+        del self.rowToCanon[row]
+        del self.valToRow[value]
 
 
-def lvn(function):
+def lvn(block, args): 
     valId = 1
     table = Table()
     var2num = {}
 
+    for arg in args:
+        var2num[arg["name"]] = valId 
+        table.insert(valId, Value.constructor2(arg["name"]), arg["name"])
+        valId += 1
+
     removeInstrs = set()
 
-    redef = [False] * len(function["instrs"])
+    redef = [False] * len(block)
     redefed = set()
-    for i, instr in reversed(enumerate(function["instrs"])):
-       if "dest" in instr and instr["dest"] in redefed:
-          redef[i] = True
-       if "dest" in instr:
-          redefed.add(instr["dest"])
+    for i, instr in reversed(list(enumerate(block))):
+        if "dest" in instr and instr["dest"] in redefed:
+            redef[i] = True
+        if "dest" in instr:
+            redefed.add(instr["dest"])
 
-    for i, instr in enumerate(function["instrs"]):
-        value = Value(instr, var2num)
+    for i, instr in enumerate(block):
+        if "label" in instr:
+            continue
+        
+        if "args" in instr:
+            for arg in instr["args"]:
+                if arg not in var2num:
+                    var2num[arg] = valId 
+                    table.insert(valId, Value.constructor2(arg), arg)
+                    valId += 1
+        if "args" in instr and instr["op"] != "const":
+            instr["args"] = [table.getFromRow(
+                var2num[a]) for a in instr["args"]]
+                
+        value = Value.constructor1(instr, var2num)
         if value.isValue:
-          if table.inTable(value):
-              num, var = table.get(value)
-              removeInstrs.add(i)
-          else:
-             num = valId
+            if instr["op"] == "call":
+                continue
+            if table.inTable(value):
+                num, var = table.get(value)
+                removeInstrs.add(i)
+            else:
+                num = valId
+                if redef[i]:
+                    dest = "var" + str(valId)
+                    var2num[instr["dest"]] = valId
+                    instr["dest"] = dest
+                else:
+                    dest = instr["dest"]
 
-             if redef[i]:
-               dest = "var" + valId
-               instr["dest"] = dest
-             else:
-               dest = instr["dest"]
+                valId += 1
+                
+                destVal = Value.constructor3(dest, instr["type"])
+                if table.inTable(destVal):
+                    table.remove(destVal)
+                table.insert(num, value, dest)
 
-             valId += 1
-
-             table.insert(num, value, dest)
-
-             if "args" in instr and instr["op"] != "const":
-              instr["args"] = [table.getFromRow(
-                  var2num[a]) for a in instr["args"]]
-
-          var2num[instr["dest"]] = num
+            var2num[instr["dest"]] = num
         else:
-           if "args" in instr:
-              instr["args"] = [table.getFromRow(
-                  var2num[a]) for a in instr["args"]]
+            if "args" in instr:
+                instr["args"] = [table.getFromRow(
+                    var2num[a]) for a in instr["args"]]
 
     newInstrs = []
-    for i, instr in enumerate(function["instrs"]):
-       if i not in removeInstrs:
-          newInstrs.append(instr)
-    function["instrs"] = newInstrs
+    for i, instr in enumerate(block):
+        if i not in removeInstrs:
+            newInstrs.append(instr)
+    block = newInstrs
+
+
+def formBBs(instrs):
+    def is_term(instr):
+        return instr["op"] == "jmp" or instr["op"] == "br"
+
+    res = []
+    cur = []
+
+    for instr in instrs:
+        if "label" in instr:
+            res.append(cur)
+            cur = [instr]
+            continue
+        cur.append(instr)
+        if is_term(instr):
+            res.append(cur)
+            cur = []
+    if cur:
+        res.append(cur)
+    return res
 
 
 if __name__ == "__main__":
     data = json.load(sys.stdin)
 
     for function in data["functions"]:
-        lvn(function)
+        blocks = formBBs(function["instrs"])
+        for block in blocks:
+            if "args" in function:
+                lvn(block, function["args"])
+            else:
+                lvn(block, [])
+        function["instrs"] = [x for sublist in blocks for x in sublist]
 
     json.dump(data, sys.stdout, indent=2)
